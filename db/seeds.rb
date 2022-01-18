e63424daf1386920472b2238b738e0f7c407a1be
Vasco Silveira <vasco.silveira@saltpay.co>
2022-01-18 21:35:51 +0000

    Added 5 seeds for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+fake_restaurants =
+  [
+    dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" },
+    pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" },
+    pizza_west =  { name: "Pizza West", address: "57A Shoreditch High St, London E1 6PQ", category: "italian" },
+    pizza_north =  { name: "Pizza North", address: "58A Shoreditch High St, London E1 6PQ", category: "italian" },
+    pizza_south =  { name: "Pizza South", address: "59A Shoreditch High St, London E1 6PQ", category: "italian" }
+  ]
+
+
+fake_restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
